0250595956c26917256f3ce36fd9d7f881e3ff07
Daniel Oleynyk <danielo@hopon.co.il>
2023-08-05 20:20:50 +0300

    added rabbitmq pubsub support

---
+++ samples/pubsub_service/pubsub_service.py
@@ -0,0 +1,21 @@
+import logging
+from typing import Callable
+
+from sakura.microservice import Microservice
+from sakura.pubsub.client import PubSubClient
+
+logger = logging.getLogger(__name__)
+
+
+class Service(metaclass=Microservice, settings_files=["samples/pubsub_service/settings.yaml"], disable_uvloop=True):
+    once: Callable
+    pubsub: dict[str, PubSubClient]
+    config: dict
+
+    @once
+    async def run(self):
+        logger.info("Started service")
+
+    @pubsub["client1"].subscribe("subscriber1")
+    async def get_from_rabbit(self, *args, **kwargs):
+        logger.info("From rabbit...")
